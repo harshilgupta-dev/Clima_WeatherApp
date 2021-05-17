07d4c302f03d738d5d2d413b0f806ad021517904
Harshil Gupta <harshilgupta0099@gmail.com>
2021-05-17 20:16:53 +0530

    create getLocation method in loading_screen.dart

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -2,6 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 
 class LoadingScreen extends StatefulWidget {
+
+  void getLocation(){
+
+  }
+
   @override
   _LoadingScreenState createState() => _LoadingScreenState();
 }
