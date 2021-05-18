e16571581f574aed903cfc6b1d803220e7abb011
Harshil Gupta <harshilgupta0099@gmail.com>
2021-05-18 23:47:41 +0530

    create apikey const for holding apiKey value

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -4,6 +4,8 @@ import 'package:http/http.dart' as http;
 import 'package:http/io_client.dart';
 import 'dart:convert';
 
+const apiKey = '2e865217e441d180a435b0901bc84401';
+
 class LoadingScreen extends StatefulWidget {
 
 
@@ -29,13 +31,13 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
   void getData() async {
 
-    http.Response response= await http.get(Uri.parse('https://api.openweathermap.org/data/2.5/weather?lat=27.8941111&lon=78.101502&appid=2e865217e441d180a435b0901bc84401'));
+    http.Response response= await http.get(Uri.parse('https://api.openweathermap.org/data/2.5/weather?lat=27.8941111&lon=78.101502&appid=$apiKey'));
 
     if(response.statusCode==200){
       String data=response.body;
 
-      int id=jsonDecode(data)['weather'][0]['id'];
-      print(id);
+      int condition=jsonDecode(data)['weather'][0]['id'];
+      print(condition);
 
       double temperature=jsonDecode(data)['main']['temp'];
       print(temperature);
