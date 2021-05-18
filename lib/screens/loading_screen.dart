77ef9846b5ef49e5fa7b5d1068efcdd82fdc2f5e
Harshil Gupta <harshilgupta0099@gmail.com>
2021-05-19 00:31:08 +0530

    optimise the code with help of networking.dart file

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,8 +1,6 @@
 import 'package:clima_weather_app/services/location.dart';
+import 'package:clima_weather_app/services/networking.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'package:http/io_client.dart';
-import 'dart:convert';
 
 const apiKey = '2e865217e441d180a435b0901bc84401';
 
@@ -15,52 +13,20 @@ class _LoadingScreenState extends State<LoadingScreen> {
   double latitude;
   double longitude;
 
-  @override
-  void initState() {
-    super.initState();
-    print('App starts');
-  }
-
-  void getLocation() async {
+  void getLocationData() async {
     Location location = Location();
     await location.getCurrentPosition();
     latitude = location.latitude;
-    longitude = location.longitude;
+    longitude = location.latitude;
 
-    getData();
-  }
-
-  void getData() async {
-    http.Response response = await http.get(Uri.parse(
-        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey'));
-
-    if (response.statusCode == 200) {
-      String data = response.body;
-
-      int condition = jsonDecode(data)['weather'][0]['id'];
-      print(condition);
-
-      double temperature = jsonDecode(data)['main']['temp'];
-      print(temperature);
-
-      String cityName = jsonDecode(data)['name'];
-      print(cityName);
-    } else {
-      print("Can't get data from link plz re-check it");
-    }
+    NetworkHelper networkHelper = NetworkHelper(
+        url:
+            'https://api.openweathermap.org/data/2.5/weather?lat=27.8941111&lon=78.101502&appid=2e865217e441d180a435b0901bc84401');
+    var weatherData = await networkHelper.getData();
   }
 
   @override
   Widget build(BuildContext context) {
-    print('App is building now');
-    getLocation();
-
     return Scaffold();
-  }
-
-  @override
-  void deactivate() {
-    super.deactivate();
-    print('App deactivate '); //when one screen close(Navigator.pop(context))
   }
 }
--- lib/services/networking.dart
+++ lib/services/networking.dart
@@ -1,9 +1,29 @@
-
-
+import 'package:clima_weather_app/services/location.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 import 'package:flutter/cupertino.dart';
 
 class NetworkHelper {
   NetworkHelper({@required this.url});
 
   final String url;
+
+  Future getData() async {
+    http.Response response = await http.get(Uri.parse(url));
+
+    if (response.statusCode == 200) {
+      String data = response.body;
+
+      int condition = jsonDecode(data)['weather'][0]['id'];
+      print(condition);
+
+      double temperature = jsonDecode(data)['main']['temp'];
+      print(temperature);
+
+      String cityName = jsonDecode(data)['name'];
+      print(cityName);
+    } else {
+      print("Can't get data from link plz re-check it");
+    }
+  }
 }
